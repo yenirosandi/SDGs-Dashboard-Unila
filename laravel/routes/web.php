c07e5ae9f696b112fd51861cbdfd859f46dd2361
yenisandi <imsandy.oh6@gmail.com>
2019-10-08 07:22:11 +0700

    Route Update!

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,10 +1,8 @@
 <?php
 //front-end:free user
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home');
 
-<<<<<<< HEAD
 //back-end:admin
 Route::get('/login', function () {
     return view('admin/login');
@@ -16,11 +14,6 @@ Route::get('/dashboard', 'AdminController@index');
 // Route::get('/master_indikator', 'sdgsIndiMaster@show');
 // Route::get('/master_sub_indikator', 'sdgsIndiSub@show');
 // Route::get('/indikator_pencapai/an_SDGs', 'sdgsIndiCapai@show');
+
+
 ?>
-=======
-
-Route::get('/', 'HomeController@index')->name('home');
-Route::get('/home', 'HomeController@index')->name('home');
-
-//Front end location//
->>>>>>> 6231bba0a7c1afdd4f568ead5e15682fdcb81e34
