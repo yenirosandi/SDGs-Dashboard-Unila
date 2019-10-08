2057e9fd299ea0b530c78051a9dbf854151fd688
Anita Dwi Maharani <anitadmhrn@gmail.com>
2019-10-08 07:13:23 +0700

    mrg

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -4,7 +4,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-<<<<<<< HEAD
 //back-end:admin
 Route::get('/login', function () {
     return view('admin/login');
@@ -16,11 +15,11 @@ Route::get('/dashboard', 'AdminController@index');
 // Route::get('/master_indikator', 'sdgsIndiMaster@show');
 // Route::get('/master_sub_indikator', 'sdgsIndiSub@show');
 // Route::get('/indikator_pencapai/an_SDGs', 'sdgsIndiCapai@show');
-?>
-=======
 
 Route::get('/', 'HomeController@index')->name('home');
 Route::get('/home', 'HomeController@index')->name('home');
+?>
+
+
 
 //Front end location//
->>>>>>> 6231bba0a7c1afdd4f568ead5e15682fdcb81e34
