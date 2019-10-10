ac19e58cb6cf9c34a87136192f88d5ddbd4975d3
Anita Dwi Maharani <anitadmhrn@gmail.com>
2019-10-10 13:52:31 +0700

    trend seeder

--- laravel/database/seeds/DatabaseSeeder.php
+++ laravel/database/seeds/DatabaseSeeder.php
@@ -13,6 +13,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(GoalsTableSeeder::class);
-        // $this->call(UsersTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
     }
 }
---
+++ laravel/database/seeds/TrendTableSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class TrendTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('t_trends')->insert([
+            [
+                'id_trend'  			=> 1,
+                'simbol_trend'  		=> 'img/sdgs/trend up.png',
+                'keterangan'		    => 'trend up',
+ 
+            ],
+            [
+                'id_trend'  			=> 2,
+                'simbol_trend'  		=> 'img/sdgs/trend better.png',
+                'keterangan'		    => 'trend better',
+ 
+            ],
+            [
+                'id_trend'  			=> 3,
+                'simbol_trend'  		=> 'img/sdgs/trend constant.png',
+                'keterangan'		    => 'trend constant',
+ 
+            ],
+            [
+                'id_trend'  			=> 4,
+                'simbol_trend'  		=> 'img/sdgs/trend down.png',
+                'keterangan'		    => 'trend down',
+ 
+            ],
+            [
+                'id_trend'  			=> 5,
+                'simbol_trend'  		=> 'img/sdgs/trend not yet.png',
+                'keterangan'		    => 'trend not yet',
+ 
+            ],
+        ]
+
+    }
+}
