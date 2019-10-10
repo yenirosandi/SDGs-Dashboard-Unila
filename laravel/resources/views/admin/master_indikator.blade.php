c6cab4c107ea44e4528651cdfafc588b5a7ee940
yenisandi <imsandy.oh6@gmail.com>
2019-10-10 19:52:37 +0700

    mekdi

--- laravel/resources/views/admin/master_indikator.blade.php
+++ laravel/resources/views/admin/master_indikator.blade.php
@@ -4,7 +4,43 @@
 @section('Judul','Master Indikator')
 @section('JudulDesc','Ini adalah halaman master indikator dimana admin dapat melihat, menambah, memperbarui, dan menghapus data master.')
 @section('content')
-  <br>
+  <!-- Form -->
+  <div class="card shadow mb-4">
+    <div class="card-header py-3">
+      <h6 class="m-0 font-weight-bold text-primary">Form Master Indikator</h6>
+    </div>
+    <div class="card-body">
+      <div class="card-body">
+        <div class="table-responsive">
+          <form class="form-horizontal" action="/action_page.php">
+            <div class="form-group">
+              <label class="control-label" for="goal">Goal ke:</label>
+              <div class="col-sm-4">
+                <select class="form-control" name="goal">
+                  @foreach($goals as $data_goals)
+                    <option value="{{$data_goals->nama_goal}}">{{$data_goals->id_goal}}</option>
+                  @endforeach
+                </select>
+              </div>
+            </div>
+            <div class="form-group">
+              <label class="control-label col-sm-2" for="indikator">Indikator:</label>
+              <div class="col-sm-6">
+                <input value="8"type="text" class="form-control" id="indikator" name="indikator" disable>
+              </div>
+            </div>
+            <div class="form-group">
+              <div class="col-sm-offset-2 col-sm-10">
+                <button type="submit" class="btn btn-primary">Tambahkan</button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Table -->
   <div class="card shadow mb-4">
     <div class="card-header py-3">
       <h6 class="m-0 font-weight-bold text-primary">Tabel Dokter</h6>
@@ -14,125 +50,33 @@
         <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
           <thead>
             <tr>
-              <th>NID</th>
-              <th>Nama</th>
-              <th>Jenis Kelamin</th>
-              <th>ID Poli</th>
+              <th>No.</th>
+              <th>Goal</th>
+              <th>Indikator</th>
               <th>Aksi</th>
             </tr>
           </thead>
           <tbody>
+            @foreach($master_indikator as $data_master)
             <tr>
-              <th>NID x</th>
-              <th>Nama x</th>
-              <th>Jenis Kelamin x</th>
-              <th>ID Poli x</th>
+              <?php $no=1; ?>
+              <th>{{$no}}<?php $no++; ?></th>
+              <th>{{$data_master->fk_id_goal}}</th>
+              <th>{{$data_master->indikator}}</th>
               <th>
-                <a href="#" class="btn btn-info btn-circle btn-sm">
-                  <i class="fas fa-user-plus"></i>
-                </a>
-                <a href="#" class="btn btn-warning btn-circle btn-sm">
+                <a href="/master_indikator/edit/{{$data_master->id_indikator}}" class="btn btn-warning btn-circle btn-sm">
                   <i class="fas fa-edit"></i>
                 </a>
-                <a href="#" class="btn btn-danger btn-circle btn-sm">
+                <a href="/master_indikator/hapus/{{$data_master->id_indikator}}" class="btn btn-danger btn-circle btn-sm">
                   <i class="fas fa-trash"></i>
                 </a>
               </th>
             </tr>
-            <tr>
-              <th>NID x</th>
-              <th>Nama x</th>
-              <th>Jenis Kelamin x</th>
-              <th>ID Poli x</th>
-              <th>
-                <a href="#" class="btn btn-info btn-circle btn-sm">
-                  <i class="fas fa-user-plus"></i>
-                </a>
-                <a href="#" class="btn btn-warning btn-circle btn-sm">
-                  <i class="fas fa-edit"></i>
-                </a>
-                <a href="#" class="btn btn-danger btn-circle btn-sm">
-                  <i class="fas fa-trash"></i>
-                </a>
-              </th>
-            </tr>
-            <tr>
-              <th>NID x</th>
-              <th>Nama x</th>
-              <th>Jenis Kelamin x</th>
-              <th>ID Poli x</th>
-
-              <th>
-                <a href="#" class="btn btn-info btn-circle btn-sm">
-                  <i class="fas fa-user-plus"></i>
-                </a>
-                <a href="#" class="btn btn-warning btn-circle btn-sm">
-                  <i class="fas fa-edit"></i>
-                </a>
-                <a href="#" class="btn btn-danger btn-circle btn-sm">
-                  <i class="fas fa-trash"></i>
-                </a>
-              </th>
-            </tr>
-            <tr>
-              <th>NID x</th>
-              <th>Nama x</th>
-              <th>Jenis Kelamin x</th>
-              <th>ID Poli x</th>
-
-              <th>
-                <a href="#" class="btn btn-info btn-circle btn-sm">
-                  <i class="fas fa-user-plus"></i>
-                </a>
-
-                <a id="myBtn" href="#" class="btn btn-warning btn-circle btn-sm">
-                  <i class="fas fa-edit"></i>
-                </a>
-
-                    <!-- <ol>
-                      <li>Mahasiswa 1</li>
-                      <li>Mahasiswa 2</li>
-                      <li>...</li>
-                      <li>Mahasiswa n</li>
-                    </ol> -->
-                </div>
-
-                <script>
-                // Get the modal
-                var modal = document.getElementById("myModal");
-
-                // Get the button that opens the modal
-                var btn = document.getElementById("myBtn");
-
-                // Get the <span> element that closes the modal
-                var span = document.getElementsByClassName("close")[0];
-
-                // When the user clicks the button, open the modal
-                btn.onclick = function() {
-                  modal.style.display = "block";
-                }
-
-                // When the user clicks on <span> (x), close the modal
-                span.onclick = function() {
-                  modal.style.display = "none";
-                }
-
-                // When the user clicks anywhere outside of the modal, close it
-                window.onclick = function(event) {
-                  if (event.target == modal) {
-                    modal.style.display = "none";
-                  }
-                }
-              </script>
-            </div>
-
-              <a href="#" class="btn btn-danger btn-circle btn-sm">
-                <i class="fas fa-trash"></i>
-              </a>
-            </th>
-          </tr>
-        </tbody>
-      </table>
+            @endforeach
+          </tbody>
+        </table>
+      </div>
     </div>
-  <br>
+  </div>
+<br>
 @endsection
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -7,25 +7,25 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 
 //back-end:admin
-
 Auth::routes();
-
 Route::group(['prefix'=>'admin','middleware'=>['auth','admin']], function(){
     Route::get('/', function () {
         return view('admin.index');
     });
-        // Route::resource('admin', 'AdminController');
-        // Route::get('/dashboard', 'AdminController@index');
-});
-
-
+    //Master Indikator
+    // Route::resource('master_indikator','sdgsIndiMasterController');
+  });
 Route::get('/logout', '\App\Http\Controllers\Auth\LoginController@logout');
 
+//Master Indikator
+Route::get('/master_indikator', 'sdgsIndiMasterController@index');
+Route::get('/master_indikator/edit/{id_indikator}', 'sdgsIndiMasterController@edit');
+Route::get('/master_indikator/hapus', 'sdgsIndiMasterController@delete');
 
 // Route::get('/profil', 'AdminController@show');
-//
 // Route::get('/sdg/{id}', 'sdgsController@show');
-Route::get('/master_indikator', 'sdgsIndiMasterController@index');
+
+
 // Route::get('/master_sub_indikator', 'sdgsIndiSub@show');
 // Route::get('/indikator_pencapai/an_SDGs', 'sdgsIndiCapai@show');
 
