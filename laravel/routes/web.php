d40076abf59bb12c1e063fbed6bf5b1115079b86
Anita Dwi Maharani <anitadmhrn@gmail.com>
2019-10-14 10:47:43 +0700

    lgout

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -16,6 +16,8 @@ Route::group(['prefix'=>'admin','middleware'=>['auth','admin']], function(){
   Route::resource('master_indikator','sdgsIndiMasterController');
   Route::resource('master_sub_indikator','sdgsSubIndikatorController');
   Route::resource('pencapaian_indikator_SDGs','sdgsCapaianIndiController');
+Route::get('/logout', '\App\Http\Controllers\Auth\LoginController@logout');
+
 
 });
 Route::get('/logout', '\App\Http\Controllers\Auth\LoginController@logout');
