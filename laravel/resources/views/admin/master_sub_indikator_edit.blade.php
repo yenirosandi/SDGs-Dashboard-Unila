c014a84e6f91a31ad9f15451fefa827d3493a0b9
Anita Dwi Maharani <anitadmhrn@gmail.com>
2019-10-15 10:00:13 +0700

    huhu:(

--- laravel/resources/views/admin/master_sub_indikator_edit.blade.php
+++ laravel/resources/views/admin/master_sub_indikator_edit.blade.php
@@ -4,39 +4,110 @@
 @section('Judul','Master Indikator')
 @section('JudulDesc','Ini adalah halaman master indikator dimana admin dapat melihat, menambah, memperbarui, dan menghapus data master.')
 @section('content')
-  <!-- Form -->
-  <!-- <div class="card shadow mb-4">
+   <!-- Form -->
+   <div class="card shadow mb-4 w-50">
     <div class="card-header py-3">
-      <h6 class="m-0 font-weight-bold text-primary">Form Master Indikator</h6>
+      <h6 class="m-0 font-weight-bold text-primary">Form Master Sub Indikator</h6>
     </div>
     <div class="card-body">
       <div class="card-body">
         <div class="table-responsive">
-          <form method="post" class="form-horizontal" action="{{route('master_indikator.update',$master_indikator->id_indikator)}}">
+          <form method="post" class="form-horizontal" action="{{route('master_sub_indikator.update',$edit_subindikators->id_m_subindikator)}}">
           {{ csrf_field() }}
-        {{ method_field('put') }}
+          {{ method_field('put') }}
             <div class="form-group">
-              <label class="control-label col-sm-8" for="goal">Goal ke:</label>
+              <label class="control-label col-sm-8" for="subindikator">Sub Indikator:</label>
+              <div class="col-sm-10">
+                <input value="{{$edit_subindikators->subindikator}}" name="subindikator" type="text" class="form-control" required>
+              </div>
+            </div>
+            <div class="form-group">
+              <label class="control-label col-sm-8" for="waktu_pengambilan">Waktu Pengambilan:</label>
+              <div class="col-sm-10">
+              <label class="form-check-inline">
+                    <input type="checkbox" name="waktu_pengambilan[]" value="jan" {{in_array("jan", $waktu_pengambilan)? "checked":""  }}>Jan
+                    </label>
+                    <label class="form-check-inline">
+                      <input type="checkbox" name="waktu_pengambilan[]" value="feb" {{in_array("feb", $waktu_pengambilan)? "checked":""  }}>Feb 
+                      </label>
+                      <label class="form-check-inline">
+                      <input type="checkbox" name="waktu_pengambilan[]" value="mar" {{in_array("mar", $waktu_pengambilan)? "checked":""  }}>Mar
+                      </label>
+                      <label class="form-check-inline">
+                      <input type="checkbox" name="waktu_pengambilan[]" value="apr" {{in_array("apr", $waktu_pengambilan)? "checked":""  }}>Apr
+                      </label>
+                      <label class="form-check-inline">
+                      <input type="checkbox" name="waktu_pengambilan[]" value="mei" {{in_array("mei", $waktu_pengambilan)? "checked":""  }}>Mei
+                      </label>
+                        <label class="form-check-inline">
+                      <input type="checkbox" name="waktu_pengambilan[]" value="jun" {{in_array("jun", $waktu_pengambilan)? "checked":""  }}>Jun 
+                      </label>
+                      <label class="form-check-inline">
+                      </label>
+                      <label class="form-check-inline">
+                        <input type="checkbox" name="waktu_pengambilan[]" value="jul" {{in_array("jul", $waktu_pengambilan)? "checked":""  }}>Jul
+                        </label>
+                        <label class="form-check-inline">
+                      <input type="checkbox" name="waktu_pengambilan[]" value="ags" {{in_array("ags", $waktu_pengambilan)? "checked":""  }}>Ags
+                      </label>
+                      <label class="form-check-inline">
+                      <input type="checkbox" name="waktu_pengambilan[]" value="sep" {{in_array("sep", $waktu_pengambilan)? "checked":""  }}>Sep
+                      </label>
+                      <label class="form-check-inline">
+                      <input type="checkbox" name="waktu_pengambilan[]" value="okt" {{in_array("okt", $waktu_pengambilan)? "checked":""  }}>Okt
+                      </label>
+                      <label class="form-check-inline">
+                      <input type="checkbox" name="waktu_pengambilan[]" value="nov" {{in_array("nov", $waktu_pengambilan)? "checked":""  }}>Nov
+                      </label>
+                      <label class="form-check-inline">
+                      <input type="checkbox" name="waktu_pengambilan[]" value="des" {{in_array("des", $waktu_pengambilan)? "checked":""  }}>Des
+                      </label>
+              </div>
+            </div>
+            <div class="form-group">
+              <label class="control-label col-sm-8" for="fk_id_indikator">Indikator: </label>
               <div class="col-sm-4">
-                <select class="form-control" name="goal">
-                  @foreach($goals as $data_goals)
-                    <option value="{{$data_goals->id_goal}}">SDG {{$data_goals->id_goal}}</option>
+                  <select name="fk_id_indikator" class="form-control">
+                  @foreach($fk_id_indikators as $key=>$value)
+                      <?php
+                      if($key!=0){
+                          $sub_fk_id_indikators=DB::table('t_m_indikator')->select('id_indikator','indikator')->where('id_indikator',$key)->get();
+                          if(count($sub_fk_id_indikators)>0){
+                              foreach ($sub_fk_id_indikators as $sub_fk_id_indikator){?>
+                                  <option value="{{$sub_fk_id_indikator->id_indikator}}"{{$edit_fk_indikators->id_indikator==$sub_fk_id_indikator->id_indikator?' selected':''}}> {{$sub_fk_id_indikator->indikator}}</option>
+                          <?php }
+                          }
+                      }
+                      ?>
                   @endforeach
                 </select>
               </div>
             </div>
             <div class="form-group">
-              <label class="control-label col-sm-8" for="indikator">Indikator:</label>
-              <div class="col-sm-8">
-                <input value="{{$master_indikator->indikator}}" name="indikator" type="text" class="form-control" required>
+              <label class="control-label col-sm-8" for="fk_id_m_sumberdata">Sumber Data: </label>
+              <div class="col-sm-4">
+                  <select name="fk_id_m_sumberdata" class="form-control">
+                  @foreach($fk_sumberdatas as $key=>$value)
+                      <?php
+                      if($key!=0){
+                          $sub_fk_sumberdatas=DB::table('t_m_sumberdata')->select('id_m_sumberdata','sumberdata')->where('id_m_sumberdata',$key)->get();
+                          if(count($sub_fk_sumberdatas)>0){
+                              foreach ($sub_fk_sumberdatas as $sub_fk_sumberdata){?>
+                                  <option value="{{$sub_fk_sumberdata->id_m_sumberdata}}"{{$edit_fk_sumberdatas->id_m_sumberdata==$sub_fk_sumberdata->id_m_sumberdata?' selected':''}}> {{$sub_fk_sumberdata->sumberdata}}</option>
+                          <?php }
+                          }
+                      }
+                      ?>
+                  @endforeach
+                </select>
               </div>
             </div>
-            <div class="form-group">
+              <div class="form-group">
               <div class="col-sm-offset-2 col-sm-10">
-                <button type="submit" class="btn btn-primary">Tambahkan</button>
+                <button type="submit" class="btn btn-primary">Ubah</button>
               </div>
             </div>
-          </form> -->
+          </form>
         </div>
       </div>
     </div>
