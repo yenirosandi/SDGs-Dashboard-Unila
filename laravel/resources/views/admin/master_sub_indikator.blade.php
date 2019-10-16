937a13fe1c57a7ad7b31b31100c29236c8fbe51d
yenisandi <imsandy.oh6@gmail.com>
2019-10-16 23:44:02 +0700

    Benerin layout doang kok

--- laravel/resources/views/admin/master_sub_indikator.blade.php
+++ laravel/resources/views/admin/master_sub_indikator.blade.php
@@ -2,10 +2,10 @@
 
 @section('title','Master Sub-Indikator SDGs')
 @section('Judul','Master Sub-Indikator')
-@section('JudulDesc','Ini adalah halaman master sub-indikator dimana admin dapat melihat, menambah, memperbarui, dan menghapus data master.')
+@section('JudulDesc','Ini adalah halaman master sub-indikator dimana admin dapat melihat, menambah, memperbarui, dan menghapus data sub indikator.')
 @section('content')
   <!-- Form -->
-  <div class="card shadow mb-4">
+  <div class="card shadow mb-4 w-50">
     <div class="card-header py-3">
       <h6 class="m-0 font-weight-bold text-primary">Form Master Sub Indikator</h6>
     </div>
@@ -14,13 +14,34 @@
         <div class="table-responsive">
           <form class="form-horizontal" method="POST" action="{{ route('master_sub_indikator.store') }}">
           @csrf
-
+            <div class="form-group">
+              <label class="control-label col-sm-8" for="fk_id_indikator">Indikator:</label>
+              <div class="col-sm-4">
+                <select class="form-control" name="fk_id_indikator">
+                  <option value="">-- Pilih Indikator --</option>
+                  @foreach($fk_id_indikators as $id=>$fk_id_indikator)
+                  <option value="{{$id}}">{{$fk_id_indikator}}</option>
+                  @endforeach
+                </select>
+              </div>
+            </div>
             <div class="form-group">
               <label class="control-label col-sm-2" for="subindikator">Sub Indikator:</label>
-              <div class="col-sm-8">
+              <div class="col-sm-10">
                 <input value=""type="text" class="form-control" id="subindikator" name="subindikator" disable>
               </div>
             </div>
+            <div class="form-group">
+            <label class="control-label col-sm-8" for="fk_id_m_sumberdata">Sumber Data:</label>
+            <div class="col-sm-4">
+              <select class="form-control" name="fk_id_m_sumberdata">
+              <option value="">-- Pilih Sumber Data --</option>
+                @foreach($fk_sumberdatas as $id=>$fk_sumberdata)
+                  <option value="{{$id}}">{{$fk_sumberdata}}</option>
+                @endforeach
+              </select>
+            </div>
+          </div>
             <div class="form-group">
               <label class="control-label col-sm-2" for="waktu_pengambilan">Waktu Pengambilan:</label>
               <div class="col-sm-8">
@@ -28,72 +49,51 @@
                         <input class="form-check-input" type="checkbox" name="waktu_pengambilan[]" value="Jan" @if(is_array(old('waktu_pengambilan')) && in_array(1, old('waktu_pengambilan'))) checked @endif> Jan
                     </label>
                     <label class="form-check-inline">
-                        <input class="form-check-input" type="checkbox" name="waktu_pengambilan[]" value="Feb" @if(is_array(old('waktu_pengambilan')) && in_array(1, old('waktu_pengambilan'))) checked @endif> Feb 
+                        <input class="form-check-input" type="checkbox" name="waktu_pengambilan[]" value="Feb" @if(is_array(old('waktu_pengambilan')) && in_array(1, old('waktu_pengambilan'))) checked @endif> Feb
                     </label>
                     <label class="form-check-inline">
                         <input class="form-check-input" type="checkbox" name="waktu_pengambilan[]" value="Mar" @if(is_array(old('waktu_pengambilan')) && in_array(1, old('waktu_pengambilan'))) checked @endif> Mar
                     </label>
                     <label class="form-check-inline">
-                        <input class="form-check-input" type="checkbox" name="waktu_pengambilan[]" value="Apr" @if(is_array(old('waktu_pengambilan')) && in_array(1, old('waktu_pengambilan'))) checked @endif> Apr 
+                        <input class="form-check-input" type="checkbox" name="waktu_pengambilan[]" value="Apr" @if(is_array(old('waktu_pengambilan')) && in_array(1, old('waktu_pengambilan'))) checked @endif> Apr
                     </label>
                     <label class="form-check-inline">
                         <input class="form-check-input" type="checkbox" name="waktu_pengambilan[]" value="Mei" @if(is_array(old('waktu_pengambilan')) && in_array(1, old('waktu_pengambilan'))) checked @endif> Mei
                     </label>
                     <label class="form-check-inline">
-                        <input class="form-check-input" type="checkbox" name="waktu_pengambilan[]" value="Jun" @if(is_array(old('waktu_pengambilan')) && in_array(1, old('waktu_pengambilan'))) checked @endif> Jun 
+                        <input class="form-check-input" type="checkbox" name="waktu_pengambilan[]" value="Jun" @if(is_array(old('waktu_pengambilan')) && in_array(1, old('waktu_pengambilan'))) checked @endif> Jun
                     </label>
                     <label class="form-check-inline">
                         <input class="form-check-input" type="checkbox" name="waktu_pengambilan[]" value="Jul" @if(is_array(old('waktu_pengambilan')) && in_array(1, old('waktu_pengambilan'))) checked @endif> Jul
                     </label>
                     <label class="form-check-inline">
-                        <input class="form-check-input" type="checkbox" name="waktu_pengambilan[]" value="Ags" @if(is_array(old('waktu_pengambilan')) && in_array(1, old('waktu_pengambilan'))) checked @endif> Ags 
+                        <input class="form-check-input" type="checkbox" name="waktu_pengambilan[]" value="Ags" @if(is_array(old('waktu_pengambilan')) && in_array(1, old('waktu_pengambilan'))) checked @endif> Ags
                     </label>
                     <label class="form-check-inline">
                         <input class="form-check-input" type="checkbox" name="waktu_pengambilan[]" value="Sep" @if(is_array(old('waktu_pengambilan')) && in_array(1, old('waktu_pengambilan'))) checked @endif> Sep
                     </label>
                     <label class="form-check-inline">
-                        <input class="form-check-input" type="checkbox" name="waktu_pengambilan[]" value="Okt" @if(is_array(old('waktu_pengambilan')) && in_array(1, old('waktu_pengambilan'))) checked @endif> Okt 
+                        <input class="form-check-input" type="checkbox" name="waktu_pengambilan[]" value="Okt" @if(is_array(old('waktu_pengambilan')) && in_array(1, old('waktu_pengambilan'))) checked @endif> Okt
                     </label>
                     <label class="form-check-inline">
                         <input class="form-check-input" type="checkbox" name="waktu_pengambilan[]" value="Nov" @if(is_array(old('waktu_pengambilan')) && in_array(1, old('waktu_pengambilan'))) checked @endif> Nov
                     </label>
                     <label class="form-check-inline">
-                        <input class="form-check-input" type="checkbox" name="waktu_pengambilan[]" value="Des" @if(is_array(old('waktu_pengambilan')) && in_array(1, old('waktu_pengambilan'))) checked @endif> Des 
+                        <input class="form-check-input" type="checkbox" name="waktu_pengambilan[]" value="Des" @if(is_array(old('waktu_pengambilan')) && in_array(1, old('waktu_pengambilan'))) checked @endif> Des
                     </label>
-              </div>
-              <div class="form-group">
-              <label class="control-label col-sm-8" for="fk_id_indikator">Indikator:</label>
-              <div class="col-sm-4">
-                <select class="form-control" name="fk_id_indikator">
-                <option value="">-- Pilih Indikator --</option> 
-                  @foreach($fk_id_indikators as $id=>$fk_id_indikator)
-                    <option value="{{$id}}">{{$fk_id_indikator}}</option>
-                  @endforeach
-                </select>
-              </div>
-              <div class="form-group">
-              <label class="control-label col-sm-8" for="fk_id_m_sumberdata">Sumber Data:</label>
-              <div class="col-sm-4">
-                <select class="form-control" name="fk_id_m_sumberdata">
-                <option value="">-- Pilih Sumber Data --</option> 
-                  @foreach($fk_sumberdatas as $id=>$fk_sumberdata)
-                    <option value="{{$id}}">{{$fk_sumberdata}}</option>
-                  @endforeach
-                </select>
-              </div>
-            </div>
-            </div>
+              </div><br>
             <div class="form-group">
               <div class="col-sm-offset-2 col-sm-10">
                 <button type="submit" class="btn btn-primary">Tambahkan</button>
               </div>
             </div>
-          
+
           </form>
         </div>
       </div>
     </div>
   </div>
+</div>
 
   <!-- Table -->
   <div class="card shadow mb-4">
@@ -137,7 +137,7 @@
                 </a>
                 <form action="{{route('master_sub_indikator.destroy',$data->id_m_subindikator)}}" method="post">
                 {{ csrf_field() }}
-                {{ method_field('delete') }}  
+                {{ method_field('delete') }}
                 <button class="btn btn-danger btn-circle btn-sm" type="submit" onclick="return confirm('Anda yakin ingin menghapus data ini?')">
                     <i class="fas fa-trash"></i>
                   </button>
--- laravel/resources/views/admin/master_sub_indikator_edit.blade.php
+++ laravel/resources/views/admin/master_sub_indikator_edit.blade.php
@@ -1,8 +1,8 @@
 @extends('layout.master_admin')
 
-@section('title','Master Indikator SDGs')
-@section('Judul','Master Indikator')
-@section('JudulDesc','Ini adalah halaman master indikator dimana admin dapat melihat, menambah, memperbarui, dan menghapus data master.')
+@section('title','Edit Master Sub-Indikator SDGs')
+@section('Judul','Edit Master Sub-Indikator')
+@section('JudulDesc','Ini adalah halaman edit master sub-indikator dimana terdapat form untuk memperbarui data master sub-indikator.')
 @section('content')
    <!-- Form -->
    <div class="card shadow mb-4 w-50">
@@ -13,22 +13,60 @@
       <div class="card-body">
         <div class="table-responsive">
           <form method="post" class="form-horizontal" action="{{route('master_sub_indikator.update',$edit_subindikators->id_m_subindikator)}}">
-          {{ csrf_field() }}
-          {{ method_field('put') }}
+            {{ csrf_field() }}
+            {{ method_field('put') }}
             <div class="form-group">
-              <label class="control-label col-sm-8" for="subindikator">Sub Indikator:</label>
+              <label class="control-label col-sm-8" for="fk_id_indikator">Indikator: </label>
+              <div class="col-sm-4">
+                  <select name="fk_id_indikator" class="form-control">
+                  @foreach($fk_id_indikators as $key=>$value)
+                      <?php
+                      if($key!=0){
+                          $sub_fk_id_indikators=DB::table('t_m_indikator')->select('id_indikator','indikator')->where('id_indikator',$key)->get();
+                          if(count($sub_fk_id_indikators)>0){
+                              foreach ($sub_fk_id_indikators as $sub_fk_id_indikator){?>
+                                  <option value="{{$sub_fk_id_indikator->id_indikator}}"{{$edit_fk_indikators->id_indikator==$sub_fk_id_indikator->id_indikator?' selected':''}}> {{$sub_fk_id_indikator->indikator}}</option>
+                          <?php }
+                          }
+                      }
+                      ?>
+                  @endforeach
+                </select>
+              </div>
+            </div>
+            <div class="form-group">
+              <label class="control-label col-sm-10" for="subindikator">Sub Indikator:</label>
               <div class="col-sm-10">
                 <input value="{{$edit_subindikators->subindikator}}" name="subindikator" type="text" class="form-control" required>
               </div>
             </div>
             <div class="form-group">
+              <label class="control-label col-sm-8" for="fk_id_m_sumberdata">Sumber Data: </label>
+              <div class="col-sm-4">
+                  <select name="fk_id_m_sumberdata" class="form-control">
+                  @foreach($fk_sumberdatas as $key=>$value)
+                      <?php
+                      if($key!=0){
+                          $sub_fk_sumberdatas=DB::table('t_m_sumberdata')->select('id_m_sumberdata','sumberdata')->where('id_m_sumberdata',$key)->get();
+                          if(count($sub_fk_sumberdatas)>0){
+                              foreach ($sub_fk_sumberdatas as $sub_fk_sumberdata){?>
+                                  <option value="{{$sub_fk_sumberdata->id_m_sumberdata}}"{{$edit_fk_sumberdatas->id_m_sumberdata==$sub_fk_sumberdata->id_m_sumberdata?' selected':''}}> {{$sub_fk_sumberdata->sumberdata}}</option>
+                          <?php }
+                          }
+                      }
+                      ?>
+                  @endforeach
+                </select>
+              </div>
+            </div>
+            <div class="form-group">
               <label class="control-label col-sm-8" for="waktu_pengambilan">Waktu Pengambilan:</label>
-              <div class="col-sm-10">
+              <div class="col-sm-8">
               <label class="form-check-inline">
                     <input type="checkbox" name="waktu_pengambilan[]" value="jan" {{in_array("jan", $waktu_pengambilan)? "checked":""  }}>Jan
                     </label>
                     <label class="form-check-inline">
-                      <input type="checkbox" name="waktu_pengambilan[]" value="feb" {{in_array("feb", $waktu_pengambilan)? "checked":""  }}>Feb 
+                      <input type="checkbox" name="waktu_pengambilan[]" value="feb" {{in_array("feb", $waktu_pengambilan)? "checked":""  }}>Feb
                       </label>
                       <label class="form-check-inline">
                       <input type="checkbox" name="waktu_pengambilan[]" value="mar" {{in_array("mar", $waktu_pengambilan)? "checked":""  }}>Mar
@@ -40,7 +78,7 @@
                       <input type="checkbox" name="waktu_pengambilan[]" value="mei" {{in_array("mei", $waktu_pengambilan)? "checked":""  }}>Mei
                       </label>
                         <label class="form-check-inline">
-                      <input type="checkbox" name="waktu_pengambilan[]" value="jun" {{in_array("jun", $waktu_pengambilan)? "checked":""  }}>Jun 
+                      <input type="checkbox" name="waktu_pengambilan[]" value="jun" {{in_array("jun", $waktu_pengambilan)? "checked":""  }}>Jun
                       </label>
                       <label class="form-check-inline">
                       </label>
@@ -63,45 +101,7 @@
                       <input type="checkbox" name="waktu_pengambilan[]" value="des" {{in_array("des", $waktu_pengambilan)? "checked":""  }}>Des
                       </label>
               </div>
-            </div>
-            <div class="form-group">
-              <label class="control-label col-sm-8" for="fk_id_indikator">Indikator: </label>
-              <div class="col-sm-4">
-                  <select name="fk_id_indikator" class="form-control">
-                  @foreach($fk_id_indikators as $key=>$value)
-                      <?php
-                      if($key!=0){
-                          $sub_fk_id_indikators=DB::table('t_m_indikator')->select('id_indikator','indikator')->where('id_indikator',$key)->get();
-                          if(count($sub_fk_id_indikators)>0){
-                              foreach ($sub_fk_id_indikators as $sub_fk_id_indikator){?>
-                                  <option value="{{$sub_fk_id_indikator->id_indikator}}"{{$edit_fk_indikators->id_indikator==$sub_fk_id_indikator->id_indikator?' selected':''}}> {{$sub_fk_id_indikator->indikator}}</option>
-                          <?php }
-                          }
-                      }
-                      ?>
-                  @endforeach
-                </select>
-              </div>
-            </div>
-            <div class="form-group">
-              <label class="control-label col-sm-8" for="fk_id_m_sumberdata">Sumber Data: </label>
-              <div class="col-sm-4">
-                  <select name="fk_id_m_sumberdata" class="form-control">
-                  @foreach($fk_sumberdatas as $key=>$value)
-                      <?php
-                      if($key!=0){
-                          $sub_fk_sumberdatas=DB::table('t_m_sumberdata')->select('id_m_sumberdata','sumberdata')->where('id_m_sumberdata',$key)->get();
-                          if(count($sub_fk_sumberdatas)>0){
-                              foreach ($sub_fk_sumberdatas as $sub_fk_sumberdata){?>
-                                  <option value="{{$sub_fk_sumberdata->id_m_sumberdata}}"{{$edit_fk_sumberdatas->id_m_sumberdata==$sub_fk_sumberdata->id_m_sumberdata?' selected':''}}> {{$sub_fk_sumberdata->sumberdata}}</option>
-                          <?php }
-                          }
-                      }
-                      ?>
-                  @endforeach
-                </select>
-              </div>
-            </div>
+            </div><br>
               <div class="form-group">
               <div class="col-sm-offset-2 col-sm-10">
                 <button type="submit" class="btn btn-primary">Ubah</button>
