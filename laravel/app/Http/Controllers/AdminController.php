194b8f899039591c2fab6d4e3a0b398ff77ac346
yenisandi <imsandy.oh6@gmail.com>
2019-11-16 00:37:17 +0700

    controller grafik

--- laravel/app/Http/Controllers/AdminController.php
+++ laravel/app/Http/Controllers/AdminController.php
@@ -1,12 +1,13 @@
 <?php
 
 namespace App\Http\Controllers;
-
-use App\Goals_model;//tambahan
-use App\Indikator_model;
-use App\Pencapaian_model;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+
+use App\Goals_model;
+use App\Indikator_model;
+use App\SubIndikator_model;
+use App\Pencapaian_model;
 use Response;
 
 
@@ -21,74 +22,48 @@ class AdminController extends Controller
     public function  linkGrafikIndi($id_indi)
     {
 
-
-      $subindi= DB::table('t_pencapaian')
-      ->join('t_m_subindikator','fk_id_m_subindikator','=','t_m_subindikator.id_m_subindikator')
-      ->select('t_pencapaian.*','t_m_subindikator.*')
-      ->where('t_pencapaian.fk_id_indikator', '=', $id_indi)
-      ->get();
-        // DD($subindi);
-
-      $tahun= DB::table('t_pencapaian')
-      ->join('t_m_subindikator','fk_id_m_subindikator','=','t_m_subindikator.id_m_subindikator')
-      ->select('t_pencapaian.nilai','t_pencapaian.tahun','t_m_subindikator.subindikator', 't_m_subindikator.fk_id_indikator')
-      ->where('t_pencapaian.fk_id_indikator', '=', $id_indi)
-      ->groupBy('subindikator', 'tahun')
-      ->orderBy('tahun')
-      ->get();
-// dd($tahun);
-
       $sub=  DB::table('t_m_subindikator')
-      ->select('t_m_subindikator.*')
+      ->join('t_goals','fk_id_goal','=','t_goals.id_goal')
+      ->join('t_m_indikator','fk_id_indikator','=','t_m_indikator.id_indikator')
+      ->select('t_m_subindikator.*','t_goals.nama_goal','t_m_indikator.indikator')
       ->where('t_m_subindikator.fk_id_indikator', '=', $id_indi)
+      ->orderBy('t_m_subindikator.id_m_subindikator')
       ->get();
       // dd($sub);
 
-      // $categories=[];
-      $data = [];
-      $name = [];
-      $data_pencapaian= [];
-      $nilai= [];
-      $dataGrafik = [];
-      foreach ($sub as $key => $data_persub){
-        $dataGrafik[$key]['name'] = $data_persub->subindikator;
-        // $dataGrafik[$key]['data'] = [rand(1,100),rand(1,100),rand(1,100),rand(1,100)];
-        $data_pencapaian=Pencapaian_model::where('fk_id_indikator', '=', $id_indi)->get();
-        foreach ($data_pencapaian as $index => $value){
-              if($data_persub->id_m_subindikator == $value->fk_id_m_subindikator){
-               $nilai=array_merge($nilai, [$value->nilai]);
-              }
-        $dataGrafik[$key]['data']= $nilai;
-        }
-
-      }  
-      
-      
+      // kakak niki
       // $dataGrafik = [];
-      // $nilai= [];
       // foreach ($sub as $key => $data_persub){
       //   $dataGrafik[$key]['name'] = $data_persub->subindikator;
-      //   $dataGrafik[$key]['data'] = $nilai;
-      //   foreach ($subindi as $key => $value){
-
-      //     if($data_persub->id_m_subindikator == $value->fk_id_m_subindikator){
-      //       $nilai[]=(int)$value->nilai;
-      //     }
-      //   }
-      
-// dd($dataGrafik);
-
-      // dd(Response::json($categories));
-      // $d=Response::json($categories);
-
-    //   foreach ($tahun as $data_persub){
-    //   $name[]= $data_persub->tahun;
-    //   $nilai[]=$data_persub->nilai;
-    //   }
-   // return json_encode($dataGrafik);
-
-     return view('admin.detail_grafik_indi', compact(
-                              'dataGrafik', 'name', 'data', 'data_pencapaian'));
+      //   $dataGrafik[$key]['data'] = [rand(1,100),rand(1,100),rand(1,100)];
+      // }
+      // dd($dataGrafik);
+      //accu nih
+      $dataGrafik = [];
+      foreach ($sub as $key => $data_persub){
+        $nilai=[];
+        $goal=$data_persub->nama_goal;
+        $indi=$data_persub->indikator;
+        $id_goal=$data_persub->fk_id_goal;
+        $subdata=$data_persub->id_m_subindikator;
+        $tahun=2017;
+        $pencapaian= DB::table('t_pencapaian')
+        ->select('t_pencapaian.*')
+        ->where('t_pencapaian.fk_id_indikator', $id_indi)
+        ->where('t_pencapaian.fk_id_m_subindikator', $subdata)
+        ->orderBy('t_pencapaian.fk_id_m_subindikator')
+        ->get();
+        // DD($pencapaian);
+        foreach ($pencapaian as $key2 => $value) {
+            $nilai[]=(int)$value->nilai;
+        }
+        $dataGrafik[$key]['name'] = $data_persub->subindikator;
+        $dataGrafik[$key]['data'] = $nilai;
+        // DD($nilai);
+      }
+      // dd($dataGrafik);
+     return view('admin.detail_grafik_indi',
+            compact('id_goal','goal','indi','dataGrafik'));
     }
 
 
@@ -116,12 +91,12 @@ class AdminController extends Controller
           ->select('t_pencapaian.*','t_goals.*','t_m_subindikator.*','t_trends.keterangan as keterangan_trend','t_trends.simbol_trend')
           ->where('t_pencapaian.fk_id_goal', '=', $id)
           // ->where('tahun','=', $tahun_capai)
-          ->orderBy('t_pencapaian.fk_id_m_subindikator')
+          ->orderBy('t_pencapaian.tahun')
           ->get();
           // DD($data_capai);
 
 
-          
+
       $goal_detail= DB::table('t_goals')->where('id_goal', $id)->get();
       return view('admin.goal_detail',
         compact('id',
@@ -137,30 +112,5 @@ class AdminController extends Controller
           'no',
           // 'sub',
           'goal_detail'));
-    }
-
-    public function store(Request $request)
-    {
-        //
-    }
-
-    public function show($id)
-    {
-        //
-    }
-
-    public function edit($id)
-    {
-        //
-    }
-
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    public function destroy($id)
-    {
-        //
     }
 }
