ec6f182ec086aa70b26360d50ee58c993dd82bfb
Anita Dwi Maharani <anitadmhrn@gmail.com>
2020-06-08 07:40:16 +0700

    ftr

--- laravel/resources/views/frontend/footer.blade.php
+++ laravel/resources/views/frontend/footer.blade.php
@@ -1,10 +1,10 @@
 <link href="{{asset('frontend/mosh/style.css')}}" rel="stylesheet">
 
   <footer class="sticky-footer bg-white">
-      <div class="container">
+      <!-- <div class="container"> -->
         <div class="copyrights">
               <p>Copyright &copy;<script>document.write(new Date().getFullYear());</script>  <b style="color:#FC8213;"> Dashboard SDGs</b>. All Rights Reserved |
               </a>SDGs Center<a style="color:#68ae00;"> <a href="https://www.unila.ac.id/" target="_blank"> Universitas Lampung </p>
             </div>
-      </div>
+      <!-- </div> -->
     </footer>
